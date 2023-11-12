9667bb12aaa0c6341ac9ca8feb888275b47ad759
Mantas Navickis <navickis@gmail.com>
2023-11-12 01:17:10 +0100

    created product page

---
+++ client/src/pages/DetailedProduct.js
@@ -0,0 +1,15 @@
+import Header from "../components/header/HeaderContainer";
+import FooterCantainer from "../components/footer/FooterContainer";
+import SingleProduct from "../components/products/SingleProduct";
+
+const DetailedProduct = () => {
+  return (
+    <div>
+      <Header/>
+      <SingleProduct/>
+      <FooterCantainer/>
+    </div>
+  );
+};
+
+export default DetailedProduct;
