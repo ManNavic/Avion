1bd1508340d2b4cf7e6491a4c05dd39ccde9c107
Mantas Navickis <navickis@gmail.com>
2023-11-12 22:27:21 +0100

    added collection route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import DetailedProduct from './pages/DetailedProduct';
+import Collections from './pages/Collections';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/products' element={<DetailedProduct />} />
+        <Route path='/collections' element={<Collections />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/Collections.js
@@ -0,0 +1,14 @@
+import FooterCantainer from '../components/footer/FooterContainer'
+import Header from '../components/header/HeaderContainer'
+import Collection from '../components/products/Collection'
+
+const Collections = () => {
+  return (
+    <>
+      <Header />
+      <Collection />
+      <FooterCantainer />
+    </>
+  )
+}
+export default Collections
