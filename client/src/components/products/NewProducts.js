42121eb7fe0074d60f9fac47b41d702342191194
Mantas Navickis <navickis@gmail.com>
2023-11-12 01:50:06 +0100

    change

--- client/src/components/products/NewProducts.js
+++ client/src/components/products/NewProducts.js
@@ -2,47 +2,57 @@ import classes from './Products.module.css'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 const NewProducts = () => {
+
   const [feedItems, setFeedItems] = useState([])
-const navigate = useNavigate()
+
+  const navigate = useNavigate()
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
           'https://long-jade-squirrel-wear.cyclic.app/products'
-        );
+        )
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
-        const data = await response.json();
-        const shuffledItems = shuffleArray(data);
-        setFeedItems(shuffledItems);
-        console.log(shuffledItems);
+        const data = await response.json()
+        const shuffledItems = shuffleArray(data)
+        setFeedItems(shuffledItems)
+        console.log(shuffledItems)
       } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+        console.error('There was a problem with the fetch operation:', error)
       }
-    };
+    }
 
-    fetchData();
-  }, []); // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchData()
+  }, []) // eslint-disable-next-line react-hooks/exhaustive-deps
 
   const shuffleArray = (array) => {
-    const shuffledArray = [...array];
+    const shuffledArray = [...array]
     for (let i = shuffledArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i]
+      ]
     }
-    return shuffledArray;
-  };
-  const handleClick=(productId)=>{
-    navigate(`/products?itemId=${productId}`);
+    return shuffledArray
+  }
+  const handleClick = (productId) => {
+    navigate(`/products?itemId=${productId}`)
     window.scrollTo(0, 0)
-}
+  }
   return (
     <div className={classes.container}>
       <h3 className={classes.title}>New Products</h3>
       <div className={classes.productContainer}>
         {feedItems.slice(0, 6).map((item) => (
-          <div key={item._id} className={classes.card} onClick={()=> handleClick(item._id)}>
+          <div
+            key={item._id}
+            className={classes.card}
+            onClick={() => handleClick(item._id)}
+          >
             <img
               src={item.imageUrl}
               alt={item.title}
@@ -56,7 +66,6 @@ const navigate = useNavigate()
       <div className={classes.buttonContainer}>
         <button className={classes.productsButton}>View collection</button>
       </div>
-      
     </div>
   )
 }
