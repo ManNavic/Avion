00d78a86d9d13ad3b469b642882488f793353859
Mantas Navickis <navickis@gmail.com>
2023-11-12 22:26:44 +0100

    added navigation

--- client/src/components/header/HeaderBot.js
+++ client/src/components/header/HeaderBot.js
@@ -1,14 +1,20 @@
 import classes from './HeaderBot.module.css'
+import { useNavigate } from 'react-router-dom';
 const HeaderBot = () =>{
+    const navigate = useNavigate()
+    const handleClick=(category)=>{
+        navigate(`/collections?category=${category}`);
+        window.scrollTo(0, 0)
+    }
     return (
         <div className={classes.container}>
-            <div className={classes.productCategoty}>Plant pots</div>
-            <div className={classes.productCategoty}>Ceramics</div>
-            <div className={classes.productCategoty}>Tables</div>
-            <div className={classes.productCategoty}>Chairs</div>
-            <div className={classes.productCategoty}> Crockery</div>
-            <div className={classes.productCategoty}>Tableware</div>
-            <div className={classes.productCategoty}>Cutlery</div>
+            <div className={classes.productCategoty} onClick={()=> handleClick('All')}> All Products</div>
+            <div className={classes.productCategoty} onClick={()=> handleClick('Pot')}>Plant pots</div>
+            <div className={classes.productCategoty} onClick={()=> handleClick('Ceramic')}>Ceramics</div>
+            <div className={classes.productCategoty} onClick={()=> handleClick('Table')}>Tables</div>
+            <div className={classes.productCategoty} onClick={()=> handleClick('Chair')}>Chairs</div>
+            <div className={classes.productCategoty} onClick={()=> handleClick('Tableware')}>Tableware</div>
+            <div className={classes.productCategoty} onClick={()=> handleClick('Cutlery')}>Cutlery</div>
         </div>
     )
 }
