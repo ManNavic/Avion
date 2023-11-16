56e33f16d2356ff3eae16fa501812c914a837679
Mantas Navickis <navickis@gmail.com>
2023-11-16 01:52:36 +0100

    added functionality on click

--- client/src/components/header/HeaderTop.js
+++ client/src/components/header/HeaderTop.js
@@ -4,14 +4,30 @@ import ProfileIcon from '../../assets/svg/Profile'
 import SearchIcon from '../../assets/svg/Search'
 import classes from './HeaderTop.module.css'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import Menu from '../login/Menu'
 
 const HeaderTop = () => {
-
+  const token = localStorage.getItem('token')
   const navigator = useNavigate()
+  const [openMenuModal, setOpenMenuModal] = useState(false)
+
+  const handleMenu = () => {
+    console.log('clicked')
+    navigator('/register')
+  }
 
   const onClick = () => {
     console.log('clicked')
     navigator('/')
+  }
+  const loggedIn = () => {
+    console.log('clicked')
+    setOpenMenuModal(true)
+    console.log(openMenuModal)
+    if (!openMenuModal) {
+      setOpenMenuModal(true)
+    } else setOpenMenuModal(false)
   }
   return (
     <>
@@ -20,19 +36,31 @@ const HeaderTop = () => {
           {' '}
           <SearchIcon />
         </div>
-        <div className={classes.mainLogo} onClick={()=>onClick()}>Avion</div>
+        <div className={classes.mainLogo} onClick={() => onClick()}>
+          Avion
+        </div>
         <div className={`${classes.icon} ${classes.hide}`}>
           <CartIcon />
         </div>
-        <div className={`${classes.icon} ${classes.hide}`}>
-          <ProfileIcon />
-        </div>
+        {token ? (
+          <div className={`${classes.icon} ${classes.hide}`} onClick={loggedIn}>
+            <ProfileIcon />
+          </div>
+        ) : (
+          <div
+            className={`${classes.icon} ${classes.hide}`}
+            onClick={handleMenu}
+          >
+            <ProfileIcon />
+          </div>
+        )}
         <div className={`${classes.icon} ${classes.hideMobile}`}>
           <SearchIcon />
         </div>
         <div className={`${classes.icon} ${classes.hideMobile}`}>
-          <MenuIcon/>
+          <MenuIcon />
         </div>
+        {openMenuModal && token && <Menu closeLogin={setOpenMenuModal} />}
       </div>
       <hr className={classes.divider}></hr>
     </>
