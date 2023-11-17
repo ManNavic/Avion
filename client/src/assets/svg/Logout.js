764cc0b716b9282cab3373e0f21375e0f603523f
Mantas Navickis <navickis@gmail.com>
2023-11-17 11:11:21 +0100

    added svgs

---
+++ client/src/assets/svg/Logout.js
@@ -0,0 +1,25 @@
+const LogoutIcon = () => {
+  return (
+    <svg
+
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 4H13V9H11.5V5.5H5.5V18.5H11.5V15H13V20H4V4Z"
+        fill="#1F2328"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M17.1332 11.25L15.3578 9.47463L16.4184 8.41397L20.0045 12L16.4184 15.586L15.3578 14.5254L17.1332 12.75H9V11.25H17.1332Z"
+        fill="#1F2328"
+      />
+    </svg>
+  )
+}
+export default LogoutIcon
