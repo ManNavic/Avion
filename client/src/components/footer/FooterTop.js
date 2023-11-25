72908200dab6e758c902d05364bdb669085175c0
Mantas Navickis <navickis@gmail.com>
2023-11-25 14:42:05 +0100

    updated footer style

--- client/src/components/footer/FooterTop.js
+++ client/src/components/footer/FooterTop.js
@@ -1,5 +1,46 @@
 import classes from './FooterTop.module.css'
+import { useState } from 'react'
 const FooterTop = () => {
+  const [formData, setFormData] = useState({
+    email: ''
+  })
+  const [error, setError] = useState([])
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch(
+        'https://long-jade-squirrel-wear.cyclic.app/subscriber',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        }
+      )
+      if (response.ok) {
+        console.log('You have subscribed successfully')
+        setFormData({
+          email: ''
+        })
+      } else {
+        const responseData = await response.json()
+
+        console.log('errordata', responseData.errors)
+        setError(responseData.errors)
+      }
+    } catch (error) {
+      console.error('Subscription failed:', error)
+    }
+  }
+  const handleInput = (e) => {
+    const { name, value } = e.target
+    setFormData((prevUser) => ({
+      ...prevUser,
+      [name]: value
+    }))
+  }
   return (
     <>
       <div className={classes.container}>
@@ -33,8 +74,17 @@ const FooterTop = () => {
         <div className={classes.form}>
           <p className={classes.formTitle}>Join our mailing list</p>
           <div className={classes.formContainer}>
-            <input placeholder="your@email.com" className={classes.email} />
-            <button className={classes.submit}>Sign up</button>
+            <form onSubmit={handleSubmit}>
+              <input
+                placeholder="your@email.com"
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInput}
+                className={classes.email}
+              />
+              <button className={classes.submit}>Sign up</button>
+            </form>
           </div>
         </div>
       </div>
