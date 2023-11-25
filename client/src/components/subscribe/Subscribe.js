cd70ca3a54489777b12434ead10ab86a82dc2b18
Mantas Navickis <navickis@gmail.com>
2023-11-25 14:42:59 +0100

    connected subscribers to db

--- client/src/components/subscribe/Subscribe.js
+++ client/src/components/subscribe/Subscribe.js
@@ -1,6 +1,47 @@
 import classes from './Subscribe.module.css'
-
+import { useState } from 'react'
 const Subscribe = () => {
+  const [formData, setFormData] = useState({
+    email: ''
+  })
+  const [error, setError] = useState([])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch(
+        'https://long-jade-squirrel-wear.cyclic.app/subscriber',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        }
+      )
+      if (response.ok) {
+        console.log('You have subscribed successfully')
+        setFormData({
+          email: ''
+        })
+      } else {
+        const responseData = await response.json()
+
+        console.log('errordata', responseData.errors)
+        setError(responseData.errors)
+      }
+    } catch (error) {
+      console.error('Subscription failed:', error)
+    }
+  }
+  const handleInput = (e) => {
+    const { name, value } = e.target
+    setFormData((prevUser) => ({
+      ...prevUser,
+      [name]: value
+    }))
+  }
   return (
     <div className={classes.container}>
       <div className={classes.form}>
@@ -12,8 +53,15 @@ const Subscribe = () => {
             Sign up for our newsletter and receive exclusive offers on new
             ranges, sales, pop up stores and more
           </p>
-          <form className={classes.inputs}>
-            <input className={classes.email} placeholder='your@email.com'/>
+          <form className={classes.inputs} onSubmit={handleSubmit}>
+            <input
+              className={classes.email}
+              placeholder="your@email.com"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInput}
+            />
             <button className={classes.submit}>Sign up</button>
           </form>
         </div>
